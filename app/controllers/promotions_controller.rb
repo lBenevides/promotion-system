445b60b7e7af221449216305d68ffeff09aa10ad
lBenevides <lhenriquebenevides@gmail.com>
2021-03-19 13:18:39 -0300

    Cadastrar uma promoçao DONE

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -6,5 +6,27 @@ class PromotionsController < ApplicationController
   def show
     @promotion = Promotion.find(params[:id]) 
   end 
+
+  def new
+    @promotion = Promotion.new
+  end
+
+  def create
+    @promotion = Promotion.new(promotion_params)
+    @promotion.save
+    
+    redirect_to @promotion
+  end
+  
+  private
+
+  def promotion_params
+    params.require(:promotion).permit(:name, 
+                                      :description,
+                                      :expiration_date,
+                                      :code,
+                                      :coupon_quantity,
+                                      :discount_rate)
+  end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#index'
 
-  resources :promotions, only: [:index, :show]
+  resources :promotions, only: [:index, :show, :new, :create]
 end
